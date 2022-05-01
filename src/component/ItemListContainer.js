07ad9095ae4520c4e02c55e7395114f31dea374b
Gabriela Chavez Veloz <gachve@hotmail.com>
2022-04-30 20:07:42 -0500

    Detalle de Producto (updated)

--- src/component/ItemListContainer.js
+++ src/component/ItemListContainer.js
@@ -1,37 +1,46 @@
 import ItemCount from "./ItemCount"
 import ItemList from "./ItemList"
 import img from "./box.png"
+import abril from "./abril.png"
+import tenis from "./tenisKitty.png"
+import botas from "./powerKitty.png"
+import sidney from "./sidney.png"
 import { useEffect, useState } from "react"
 
+
 const productList = [
-    {
-      id: 1,
-      title:"producto title 1",
-      description: "producto 1",
-      price: 100,
-      pictureUrl: img
-    },
-    {
-      id: 2,
-      title:"producto title 2",
-      description: "producto 2",
-      price: 200,
-      pictureUrl: img
-    },
-    {
-      id: 3,
-      title:"producto title 3",
-      description: "producto 3",
-      price: 300,
-      pictureUrl: img
-    },
-    {
-      id: 4,
-      title:"producto title 4",
-      description: "producto 4",
-      price: 400,
-      pictureUrl: img
-      }
+  {
+    id: 1,
+    title:"Abril",
+    description: "Bonitos zapatos tipo flats en color negro con cara de gato y bigotes dorados, con comoda plantilla",
+    price: 100,
+    category: ["flats","cat"],
+    pictureUrl:abril
+  },
+  {
+    id: 2,
+    title:"Tenis Kitty",
+    description: "Comodos y bonitos tenis con estampado de Kitty y suela ancha",
+    price: 200,
+    category: ["tenis","kitty"],
+    pictureUrl: tenis
+  },
+  {
+    id: 3,
+    title:"Power Kitty",
+    description: "Bonitas botas en color negro con estampado de Kitty, se pueden combinar con todo!",
+    price: 300,
+    category: ["botas","kitty"],
+    pictureUrl: botas
+  },
+  {
+    id: 4,
+    title:"sidney",
+    description: "Bonitos zapatos tipo flats en color beige con cara de gato, con comoda plantilla",
+    price: 400,
+    category: ["flats","cat"],
+    pictureUrl: sidney
+    }
   ]
 
    const promesa = new Promise((res)=>{
@@ -64,7 +73,7 @@ const ItemListContainer = ({greeting}) => {
           <h2>{greeting}</h2>
         </div>
         {/* <ItemCount stock={10} init={1} onAdd={onAdd}/> */}
-        <ItemList productList={productList}/>
+        <ItemList productList={productos}/>
 
     </>
     
